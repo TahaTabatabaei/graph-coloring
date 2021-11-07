2a8fac2e080f8bdf054f4f84c916c40b411c71cb
seyed mohammad taha tabatabaei <78843420+TahaTabatabaei@users.noreply.github.com>
2021-11-07 19:19:10 +0330

    adding files

---
+++ src/IDS.java
@@ -0,0 +1,83 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Stack;
+
+public class IDS {
+
+    public static boolean search(State initialState, int cutoff){
+        Stack<State> frontier = new Stack<State>();
+        Hashtable<String, Boolean> inFrontier = new Hashtable<>();
+        Hashtable<String, Boolean> explored = new Hashtable<>();
+        if(isGoal(initialState)){
+            result(initialState);
+            return true;
+        }
+        frontier.add(initialState);
+        inFrontier.put(initialState.hash(),true);
+        while (!frontier.isEmpty()){
+            State tempState = frontier.pop();
+            inFrontier.remove(tempState.hash());
+            explored.put(tempState.hash(),true);
+            if (tempState.getDepth() >= cutoff) {
+                continue;
+            }
+            ArrayList<State> children = tempState.successor();
+            for (int i = 0; i < children.size(); i++) {
+                if (!(inFrontier.containsKey(children.get(i).hash()))
+                        && !(explored.containsKey(children.get(i).hash()))) {
+                    if (isGoal(children.get(i))) {
+                        result(children.get(i));
+                        return true;
+                    }
+                    frontier.add(children.get(i));
+                    inFrontier.put(children.get(i).hash(), true);
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isGoal(State state){
+        for (int i = 0; i < state.getGraph().size(); i++) {
+            if(state.getGraph().getNode(i).getColor() == Color.Red
+                    || state.getGraph().getNode(i).getColor() == Color.Black){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void result(State state){
+        Stack<State> states = new Stack<State>();
+        while (true){
+            states.push(state);
+            if(state.getParentState() == null){
+                break;
+            }
+            else {
+                state = state.getParentState();
+            }
+        }
+        try {
+            FileWriter myWriter = new FileWriter("BfsResult.txt");
+            System.out.println("initial state : ");
+            while (!states.empty()){
+                State tempState = states.pop();
+                if(tempState.getSelectedNodeId() != -1) {
+                    System.out.println("selected id : " + tempState.getSelectedNodeId());
+                }
+                tempState.getGraph().print();
+
+                myWriter.write(tempState.getSelectedNodeId()+" ,");
+                myWriter.write(tempState.outputGenerator()+"\n");
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
